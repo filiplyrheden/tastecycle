1fbb58d4a0c93cafc9158e0f3b1509893a1205cd
filiplyrheden <filip.lyrheden@gmail.com>
2025-10-31 11:33:09 +0100

    Wrap RootNavigator in GluestackUIProvider and update package dependencies for Gluestack UI

--- App.tsx
+++ App.tsx
@@ -61,7 +61,9 @@ export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <RootNavigator />
+        <GluestackUIProvider>
+          <RootNavigator />
+        </GluestackUIProvider>
       </NavigationContainer>
     </AuthProvider>
   );
